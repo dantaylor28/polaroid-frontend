2d09f65378e9f4f1cb42a9b0828b475e8bb4ef5f
Dan <dan.taylor1493@gmail.com>
2025-10-08 09:56:48 +0200

    Declare basic token utilities

--- polaroid-frontend/src/api/axios.js
+++ polaroid-frontend/src/api/axios.js
@@ -11,3 +11,16 @@ const axiosInstance = axios.create({
   withCredentials: true,
 });
 
+// Token Utilities
+const getAccessToken = () => localStorage.getItem("access_token");
+const getRefreshToken = () => localStorage.getItem("refresh_token");
+
+export const setTokens = (access, refresh) => {
+  localStorage.setItem("access_token", access);
+  localStorage.setItem("refresh_token", refresh);
+};
+
+export const clearTokens = () => {
+  localStorage.removeItem("access_token");
+  localStorage.removeItem("refresh_token");
+};
