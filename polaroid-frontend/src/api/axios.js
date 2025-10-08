b9d049f359e208b275d71a5b51a95cd6613b75fe
Dan <dan.taylor1493@gmail.com>
2025-10-08 09:30:05 +0200

    Create a custom axios instance

--- polaroid-frontend/src/api/axios.js
+++ polaroid-frontend/src/api/axios.js
@@ -1,4 +1,13 @@
 import axios from "axios";
 import jwtDecode from "jwt-decode";
 
-const BASE_URL = "add live api url here when deployed through render"
+const BASE_URL = "add live api url here when deployed through render";
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
