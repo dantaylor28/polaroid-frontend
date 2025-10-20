523062b115e299bd07633bfbe0f905c42e6f9e88
Dan <dan.taylor1493@gmail.com>
2025-10-20 17:07:15 +0200

    Create simple login form in Login.jsx file

--- polaroid-frontend/src/pages/Login.jsx
+++ polaroid-frontend/src/pages/Login.jsx
@@ -8,5 +8,33 @@ export const Login = () => {
 
   const [signInData, setSignInData] = useState({ username: "", password: "" });
   const [errors, setErrors] = useState({});
-  return <div>Login</div>;
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <div>
+        <form onSubmit={() => {}} className="flex flex-col gap-3">
+          <input
+            type="text"
+            name="username"
+            placeholder="username"
+            value={form.username}
+            onChange={() => {}}
+            className="border p-2"
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            value={form.password}
+            onChange={() => {}}
+            className="border p-2"
+          />
+          {errors && <p className="text-red-500 text-sm">{errors}</p>}
+          <button type="submit">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
