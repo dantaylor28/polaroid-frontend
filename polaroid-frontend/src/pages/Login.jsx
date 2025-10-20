96de7c5662a5f43d497fe277685b9beacf18fca0
Dan <dan.taylor1493@gmail.com>
2025-10-20 17:46:56 +0200

    Define and call handleSubmit function in Login.jsx

--- polaroid-frontend/src/pages/Login.jsx
+++ polaroid-frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import axiosInstance from "../api/axios";
 
 export const Login = () => {
   const navigate = useNavigate();
@@ -12,11 +13,33 @@ export const Login = () => {
   const handleChange = (e) => {
     setSignInData({ ...signInData, [e.target.name]: e.target.value });
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setErrors({});
+
+    try {
+      const { data } = await axiosInstance.post(
+        "/dj-rest-auth/login/",
+        signInData
+      );
+
+      //   expect { access_token, refresh_token, user: {...} }
+      login(data.access_token, data.refresh_token, data.user);
+
+      //   Redirect user
+      navigate(-1);
+    } catch (error) {
+      console.log("Login Error:", error.response?.data || error.message);
+      setErrors(error.response?.data || { non_field_errors: ["Login Failed"] });
+    }
+  };
+  
   return (
     <div>
       <h1>Login Page</h1>
       <div>
-        <form onSubmit={() => {}} className="flex flex-col gap-3">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
           <input
             type="text"
             name="username"
