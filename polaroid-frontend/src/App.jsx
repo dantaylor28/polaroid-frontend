51b2bf9bffa485f2e06d4c8f94b58c2dde266581
Dan <dan.taylor1493@gmail.com>
2025-11-18 17:18:21 +0100

    Reorganise file structure & create GuestRoute.jsx file

--- polaroid-frontend/src/App.jsx
+++ polaroid-frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
-import { Login } from "./pages/Login";
+import { Login } from "./pages/auth/Login";
 import { Profile } from "./pages/Profile";
-import { ProtectedRoute } from "./auth/ProtectedRoute";
+import { ProtectedRoute } from "./pages/auth/ProtectedRoute";
 import { AuthProvider } from "./context/AuthContext";
 
 function App() {
--- polaroid-frontend/src/pages/auth/Login.jsx
+++ polaroid-frontend/src/pages/auth/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
-import axiosInstance from "../api/axios";
+import { useAuth } from "../../context/AuthContext";
+import axiosInstance from "../../api/axios";
 
 export const Login = () => {
   const navigate = useNavigate();
