1981cbd08a7fcc7b48a830f65b0edd101c89c968
Dan <dan.taylor1493@gmail.com>
2025-11-20 07:28:23 +0100

    Create signup page and move currentUser div to home page

--- polaroid-frontend/src/pages/Home.jsx
+++ polaroid-frontend/src/pages/Home.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import { useAuth } from "../context/AuthContext";
 
 export const Home = () => {
+  const { currentUser } = useAuth();
   return (
-    <div>Home page.. Landing page</div>
-  )
-}
+    <div>
+      Home page.. Landing page
+      <div>
+        {!currentUser ? (
+          <p>Not logged in</p>
+        ) : (
+          <h1 className="text-blue-500">Welcome, {currentUser.username}</h1>
+        )}
+      </div>
+    </div>
+  );
+};
--- polaroid-frontend/src/pages/Profile.jsx
+++ polaroid-frontend/src/pages/Profile.jsx
@@ -1,10 +1,5 @@
-import { useAuth } from "../context/AuthContext";
+
 
 export const Profile = () => {
-  const { currentUser } = useAuth();
-  return <div>Profile Page here.. protected route
-    <div>
-      {!currentUser ? <p>Not logged in</p> : <h1 className="text-blue-500">Welcome, {currentUser.username}</h1>}
-    </div>
-  </div>;
+  return <div>Profile Page here.. protected route</div>;
 };
