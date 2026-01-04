eb5bf98218290f23997433f28eb134909b500ce0
Dan <dan.taylor1493@gmail.com>
2026-01-04 14:24:55 +0100

    Wrap App in ProfileProvider in main.jsx

--- polaroid-frontend/src/main.jsx
+++ polaroid-frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { ProfileProvider } from "./context/ProfileContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <ProfileProvider>
+        <App />
+      </ProfileProvider>
     </AuthProvider>
   </StrictMode>
 );
