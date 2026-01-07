275a77a196caa343acf5f5f29aca1ddce9615120
Dan <dan.taylor1493@gmail.com>
2026-01-07 16:54:25 +0100

    Add useEffect to fetch user profiles on Profile page

--- polaroid-frontend/src/pages/Profile.jsx
+++ polaroid-frontend/src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axiosInstance from "../api/axios";
 
 export const Profile = () => {
   const { username } = useParams();
@@ -10,7 +11,34 @@ export const Profile = () => {
   const [loading, setLoading] = useState(true);
 
   // Determine whose profile is being viewed
-  const profileUsername = username || currentUser?.username
-  const isSelf = profileUsername === currentUser?.username
+  const profileUsername = username || currentUser?.username;
+  const isSelf = profileUsername === currentUser?.username;
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const { data } = await axiosInstance.get(
+          `/profiles/${profileUsername}/`
+        );
+        setProfile(data);
+      } catch (error) {
+        console.log("Failed to load profile", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (profileUsername) {
+      fetchProfile();
+    }
+  }, [profileUsername]);
+
+  if (loading) {
+    return <p className="px-6 py-4 text-sm">Loading Profile...</p>;
+  }
+
+  if (!profile) {
+    return <p className="px-6 py-4 text-sm">Profile not found</p>;
+  }
   return <div></div>;
 };
