496fa2ccf3a4ba4002ade72f8b549377852cfe75
Dan <dan.taylor1493@gmail.com>
2026-03-17 08:43:07 +0100

    Add logic to CompressImage.js

--- polaroid-frontend/src/utils/CompressImage.js
+++ polaroid-frontend/src/utils/CompressImage.js
@@ -1,5 +1,17 @@
-import React from 'react'
+import imageCompression from "browser-image-compression";
 
-export const CompressImage = () => {
-    
-}
+export const CompressImage = async () => {
+  const options = {
+    maxSizeMB: 1,
+    maxWidthOrHeight: 2000,
+    useWebWorker: true,
+  };
+
+  try {
+    const compressedFile = await imageCompression(file, options);
+    return compressedFile;
+  } catch (error) {
+    console.error("Image compression failed:", error);
+    return file;
+  }
+};
