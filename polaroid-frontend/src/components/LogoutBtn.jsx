b426e762364e4b5a222329f80cc093f999c3aa9e
Dan <dan.taylor1493@gmail.com>
2026-04-27 13:57:20 +0200

    Move all of my state and logout logic from LogoutBtn to NavBar.jsx

--- polaroid-frontend/src/components/LogoutBtn.jsx
+++ polaroid-frontend/src/components/LogoutBtn.jsx
@@ -1,41 +1,14 @@
-import { useAuth } from "../context/AuthContext";
-import { useNavigate } from "react-router-dom";
-import { ConfirmModal } from "../utils/ConfirmModal";
-import toast from "react-hot-toast";
-import { useState } from "react";
 import { LogOut } from "lucide-react";
 
-export const LogoutBtn = () => {
-  const { logout } = useAuth();
-  const navigate = useNavigate();
-  const [openLogoutConfirm, setOpenLogoutConfirm] = useState(false);
-
-  const confirmLogout = () => {
-    logout();
-    toast("Logged out successfully", {
-      icon: "👋",
-    });
-    navigate("/login");
-    setOpenLogoutConfirm(false);
-  };
+export const LogoutBtn = ({ onClick }) => {
   return (
     <>
       <button
-        onClick={() => setOpenLogoutConfirm(true)}
         className="px-3.5 py-1.5 rounded-full transition text-black hover:bg-red-500 hover:text-white hover:cursor-pointer"
+        onClick={onClick}
       >
         <LogOut />
       </button>
-
-      <ConfirmModal
-        open={openLogoutConfirm}
-        title="Are you sure you want to log out?"
-        description="You'll need to log back in to access your account."
-        confirmText="Logout"
-        onConfirm={confirmLogout}
-        onCancel={() => setOpenLogoutConfirm(false)}
-        variant="danger"
-      />
     </>
   );
 };
--- polaroid-frontend/src/components/NavBar.jsx
+++ polaroid-frontend/src/components/NavBar.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink, useLocation } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { LogoutBtn } from "./LogoutBtn";
-import { House, SquarePlus } from "lucide-react";
+import { House } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import { ConfirmModal } from "../utils/ConfirmModal";
+import toast from "react-hot-toast";
 
 // export const NavBar = () => {
 //   const { currentUser } = useAuth();
@@ -94,65 +97,81 @@ export const NavBar = () => {
   const { currentUser } = useAuth();
   const { pathname } = useLocation();
   const isAuthPage = pathname === "/login" || pathname === "/signup";
+  const [openLogoutConfirm, setOpenLogoutConfirm] = useState(false);
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  const confirmLogout = () => {
+    logout();
+    toast("Logged out successfully", {
+      icon: "👋",
+    });
+    navigate("/login");
+    setOpenLogoutConfirm(false);
+  };
 
   return (
-    <nav className="fixed top-0 left-0 w-full z-50 bg-transparent">
-      {/* Centered container */}
-      <div className="max-w-7xl mx-auto px-8 p-8">
-        <div className="flex items-center justify-between">
-          {/* Logo */}
-          <Link to="/" className="text-2xl font-bold text-blue-700 px-3.5">
-            Polaroid
-          </Link>
+    <>
+      <nav
+        className={` fixed top-0 left-0 w-full z-50 ${isAuthPage ? "bg-transparent" : "bg-white/40 backdrop-blur-xl border-b border-white/20 shadow-sm"}
+  `}
+      >
+        {/* Centered container */}
+        <div className="max-w-7xl mx-auto px-8 py-6">
+          <div className="flex items-center justify-between">
+            {/* Logo */}
+            <Link to="/" className="text-2xl font-bold text-blue-700 px-3.5">
+              Polaroid
+            </Link>
 
-          {/* Nav Links */}
-          <div className="flex items-center gap-4">
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                `px-3.5 py-1.5 rounded-full transition ${
-                  isAuthPage
-                    ? isActive
-                      ? "text-white bg-white/30"
-                      : "text-white/50 bg-white/10 hover:text-white hover:bg-white/20"
-                    : isActive
-                      ? "bg-gray-100 text-black"
-                      : "text-gray-700 hover:bg-gray-50 hover:text-black"
-                }`
-              }
-            >
-              <House />
-            </NavLink>
-
-            {currentUser ? (
-              <>
-                <NavLink
-                  to="/profile"
-                  end
-                  className={({ isActive }) =>
-                    `flex items-center gap-2 px-3 py-1 rounded-full transition font-medium ${
-                      isActive
+            {/* Nav Links */}
+            <div className="flex items-center gap-4">
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  `px-3.5 py-1.5 rounded-full transition ${
+                    isAuthPage
+                      ? isActive
+                        ? "text-white bg-white/30"
+                        : "text-white/50 bg-white/10 hover:text-white hover:bg-white/20"
+                      : isActive
                         ? "bg-gray-100 text-black"
                         : "text-gray-700 hover:bg-gray-50 hover:text-black"
-                    }`
-                  }
-                >
-                  <img
-                    src={currentUser.profile_image}
-                    alt="Profile"
-                    className="w-7 h-7 rounded-full object-cover"
-                  />
-                  {currentUser.username}
-                </NavLink>
+                  }`
+                }
+              >
+                <House />
+              </NavLink>
 
-                <LogoutBtn />
-              </>
-            ) : (
-              <>
-                <NavLink
-                  to="/login"
-                  className={({ isActive }) =>
-                    `px-3.5 py-1.5 rounded-full transition font-medium 
+              {currentUser ? (
+                <>
+                  <NavLink
+                    to="/profile"
+                    end
+                    className={({ isActive }) =>
+                      `flex items-center gap-2 px-3 py-1 rounded-full transition font-medium ${
+                        isActive
+                          ? "bg-gray-100 text-black"
+                          : "text-gray-700 hover:bg-gray-50 hover:text-black"
+                      }`
+                    }
+                  >
+                    <img
+                      src={currentUser.profile_image}
+                      alt="Profile"
+                      className="w-7 h-7 rounded-full object-cover"
+                    />
+                    {currentUser.username}
+                  </NavLink>
+
+                  <LogoutBtn onClick={() => setOpenLogoutConfirm(true)} />
+                </>
+              ) : (
+                <>
+                  <NavLink
+                    to="/login"
+                    className={({ isActive }) =>
+                      `px-3.5 py-1.5 rounded-full transition font-medium 
                     ${
                       isAuthPage
                         ? isActive
@@ -162,15 +181,15 @@ export const NavBar = () => {
                           ? "text-gray-700 hover:bg-gray-50 hover:text-black"
                           : ""
                     }`
-                  }
-                >
-                  Login
-                </NavLink>
+                    }
+                  >
+                    Login
+                  </NavLink>
 
-                <NavLink
-                  to="/signup"
-                  className={({ isActive }) =>
-                    `px-3.5 py-1.5 rounded-full transition font-medium
+                  <NavLink
+                    to="/signup"
+                    className={({ isActive }) =>
+                      `px-3.5 py-1.5 rounded-full transition font-medium
                   ${
                     isAuthPage
                       ? isActive
@@ -180,22 +199,32 @@ export const NavBar = () => {
                         ? "text-gray-700 hover:bg-gray-50 hover:text-black"
                         : ""
                   }`
-                  }
-                >
-                  Signup
-                </NavLink>
-              </>
-            )}
+                    }
+                  >
+                    Signup
+                  </NavLink>
+                </>
+              )}
+            </div>
           </div>
+          <div
+            className={`mx-auto h-px mt-6 ${
+              isAuthPage
+                ? "bg-[linear-gradient(to_right,black_50%,white_50%)] opacity-20"
+                : "bg-black/20"
+            }`}
+          />
         </div>
-        <div
-          className={`mx-auto h-px mt-6 ${
-            isAuthPage
-              ? "bg-[linear-gradient(to_right,black_50%,white_50%)] opacity-20"
-              : "bg-black/20"
-          }`}
-        />
-      </div>
-    </nav>
+      </nav>
+      <ConfirmModal
+        open={openLogoutConfirm}
+        title="Are you sure you want to log out?"
+        description="You'll need to log back in to access your account."
+        confirmText="Logout"
+        onConfirm={confirmLogout}
+        onCancel={() => setOpenLogoutConfirm(false)}
+        variant="danger"
+      />
+    </>
   );
 };
