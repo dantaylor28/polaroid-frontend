dc10726a980ea92f9db0a9912a136cf7ebfbc69c
Dan <dan.taylor1493@gmail.com>
2026-06-04 08:00:09 +0200

    Add overlay & likes/pins counter to images on home page

--- polaroid-frontend/src/pages/Home.jsx
+++ polaroid-frontend/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { useAuth } from "../context/AuthContext";
 import { CreatePostModal } from "../components/CreatePostModal";
 import axiosInstance from "../api/axios";
 import { PostDetailsModal } from "../components/PostDetailsModal";
+import { Heart, Pin } from "lucide-react";
 
 export const Home = () => {
   const { currentUser } = useAuth();
@@ -46,15 +47,29 @@ export const Home = () => {
 
             <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-4 p-4">
               {posts.map((post) => (
-                <div key={post.id} className="mb-4 break-inside-avoid">
+                <div
+                  key={post.id}
+                  className="cursor-pointer mb-4 break-inside-avoid relative group"
+                  onClick={() => {
+                    setSelectedPost(post);
+                  }}
+                >
                   <img
                     src={post.post_image}
                     alt="Post image"
-                    className="w-full rounded-lg cursor-pointer"
-                    onClick={() => {
-                      setSelectedPost(post);
-                    }}
+                    className="w-full rounded-lg"
                   />
+
+                  <div className="absolute inset-0 bg-black/40 opacity-0 group-hover:opacity-100 transition-opacity duration-200 rounded-lg flex items-end justify-center text-white/90 pb-2">
+                    <div className="flex gap-1.5">
+                      <Heart className="size-5" />
+                      <span className="text-sm">0</span>
+                    </div>
+                    <div className="flex gap-1.5 ml-8">
+                      <Pin className="size-5" />
+                      <span className="text-sm">0</span>
+                    </div>
+                  </div>
                 </div>
               ))}
             </div>
