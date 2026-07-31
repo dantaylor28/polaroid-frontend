14e481d7f749775df1eda6867e820191e8560c17
Dan <dan.taylor1493@gmail.com>
2026-07-31 19:55:03 +0200

    Set states and debounce in Serach.jsx page

--- polaroid-frontend/src/pages/Search.jsx
+++ polaroid-frontend/src/pages/Search.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState } from "react";
+import SearchBar from "../components/SearchBar";
+import { useDebounce } from "../hooks/useDebounce";
 
 export const Search = () => {
+  const [query, setQuery] = useState("");
+  const debouncedQuery = useDebounce(query, 300);
   return (
-    <div>Search page</div>
-  )
-}
+    <div className="max-w-3xl mx-auto mt-32 px-6">
+      <SearchBar
+        placeholder="What are you searching for?"
+        value={query}
+        onChange={setQuery}
+      />
+    </div>
+  );
+};
