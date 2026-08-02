2685c639ec688ac4c36a32304f1dcccaba7183de
Dan <dan.taylor1493@gmail.com>
2026-08-02 09:46:29 +0200

    Get my search functionality working with a dynamic tab menu

--- polaroid-frontend/src/pages/Search.jsx
+++ polaroid-frontend/src/pages/Search.jsx
@@ -4,6 +4,7 @@ import { useDebounce } from "../hooks/useDebounce";
 import axiosInstance from "../api/axios";
 import { Link } from "react-router-dom";
 import { PostGrid } from "../components/PostGrid";
+import { LayoutDashboard, Users } from "lucide-react";
 
 export const Search = () => {
   const [query, setQuery] = useState("");
@@ -12,6 +13,8 @@ export const Search = () => {
   const [selectedPost, setSelectedPost] = useState(null);
   const [loading, setLoading] = useState(false);
   const debouncedQuery = useDebounce(query, 300);
+
+  const [activeTab, setActiveTab] = useState("profiles");
 
   useEffect(() => {
     if (!debouncedQuery.trim()) {
@@ -40,6 +43,10 @@ export const Search = () => {
 
     fetchResults();
   }, [debouncedQuery]);
+
+  const handleTabChange = async (tab) => {
+    setActiveTab(tab);
+  };
   return (
     <div className="max-w-3xl mx-auto mt-32 px-6">
       <SearchBar
@@ -48,50 +55,67 @@ export const Search = () => {
         onChange={setQuery}
       />
 
-      <div className="mt-6">
-        {/* Users */}
-        <section>
-          <h2 className="text-lg font-semibold mb-4">Users</h2>
+      {debouncedQuery && (
+        <>
+          <div className="mt-6">
+            <div className="relative flex items-center justify-center mt-8">
+              <button
+                onClick={() => handleTabChange("profiles")}
+                className={`flex-1 flex justify-center py-3 cursor-pointer ${activeTab === "profiles" ? "text-black" : "text-black/40"}`}
+              >
+                <Users className="hover:text-black/70" />
+              </button>
 
-          {profiles.length === 0 ? (
-            <p className="text-sm text-black/50">No users found.</p>
-          ) : (
-            <div className="space-y-3">
-              {profiles.map((profile) => (
-                <Link
-                  key={profile.id}
-                  to={`/profile/${profile.owner}`}
-                  className="flex items-center gap-3 rounded-lg p-2 hover:bg-gray-50 transition"
-                >
-                  <img
-                    src={profile.profile_image}
-                    alt={profile.owner}
-                    className="w-10 h-10 rounded-full object-cover"
-                  />
+              <button
+                onClick={() => handleTabChange("posts")}
+                className={`flex-1 flex justify-center py-3 cursor-pointer ${activeTab === "posts" ? "text-black" : "text-black/40"}`}
+              >
+                <LayoutDashboard className="hover:text-black/70" />
+              </button>
 
-                  <div>
-                    <p className="font-medium">{profile.owner}</p>
-                    <p className="text-sm text-black/50">
-                      {profile.bio || "No bio"}
-                    </p>
-                  </div>
-                </Link>
-              ))}
+              <span
+                className={`absolute bottom-0 h-px w-1/2 transition-all bg-black/30 duration-400 ${activeTab === "profiles" ? "left-0" : "left-1/2"}`}
+              />
             </div>
-          )}
-        </section>
 
-        {/* Posts */}
-        <section className="mt-10">
-          <h2 className="text-lg font-semibold mb-4">Posts</h2>
-
-          {posts.length === 0 ? (
-            <p className="text-sm text-black/50">No posts found.</p>
-          ) : (
-            <PostGrid posts={posts} onSelectPost={setSelectedPost} />
-          )}
-        </section>
-      </div>
+            {activeTab === "profiles" ? (
+              profiles.length === 0 ? (
+                <p className="py-12 text-center text-sm text-black/50">
+                  No users found.
+                </p>
+              ) : (
+                <div className="space-y-3 mt-6">
+                  {profiles.map((profile) => (
+                    <Link
+                      key={profile.id}
+                      to={`/profile/${profile.owner}`}
+                      className="flex items-center gap-3 rounded-lg p-2 hover:bg-gray-50 transition"
+                    >
+                      <img
+                        src={profile.profile_image}
+                        alt={profile.owner}
+                        className="w-10 h-10 rounded-full object-cover"
+                      />
+                      <div>
+                        <p className="font-medium">{profile.owner}</p>
+                        <p className="text-sm text-black/50">
+                          {profile.bio || "No Bio"}
+                        </p>
+                      </div>
+                    </Link>
+                  ))}
+                </div>
+              )
+            ) : posts.length === 0 ? (
+              <p className="py-12 text-center text-sm text-black/50">
+                No posts found.
+              </p>
+            ) : (
+              <PostGrid posts={posts} onSelectPost={setSelectedPost} />
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 };
