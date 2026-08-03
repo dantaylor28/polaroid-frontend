18942585cd2502333b1798678391802149a948a6
Dan <dan.taylor1493@gmail.com>
2026-08-03 10:49:27 +0200

    Add an X icon to my searchBar component to clear values onClick

--- polaroid-frontend/src/components/SearchBar.jsx
+++ polaroid-frontend/src/components/SearchBar.jsx
@@ -1,16 +1,25 @@
+import { X } from "lucide-react";
 
-
-const SearchBar = ({value, onChange, placeholder = "Search"}) => {
+const SearchBar = ({ value, onChange, placeholder = "Search" }) => {
   return (
-    <div className="px-3 pb-2">
+    <div className="flex items-center pb-2 relative px-3">
       <input
         type="text"
         value={value}
         onChange={(e) => onChange(e.target.value)}
         placeholder={placeholder}
-        className="w-full rounded-lg border border-black/10 px-3 py-2 text-sm focus:outline-none focus:ring-2 
+        className="w-full rounded-lg border border-black/10 px-3 pr-10 py-2 text-sm focus:outline-none focus:ring-2 
           focus:ring-blue-500/20 placeholder:text-black/40"
       />
+      {value && (
+        <button
+          type="button"
+          className="absolute right-5 mr-2 flex items-center justify-center"
+          onClick={() => onChange("")}
+        >
+          <X className="size-5 text-black/30 hover:text-black/60 transition cursor-pointer" />
+        </button>
+      )}
     </div>
   );
 };
